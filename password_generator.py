717791559f615ba783f360754911c4aa725a5c3b
Mohsinpathan17 <135431762+Mohsinpathan17@users.noreply.github.com>
2025-04-27 19:45:23 +0530

    password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -5,19 +5,16 @@ def generate_password(length=12, use_digits=True, use_specials=True):
     letters = string.ascii_letters
     digits = string.digits
     specials = string.punctuation
-
-    characters = letters
+   characters = letters
     if use_digits:
         characters += digits
+        
     if use_specials:
         characters += specials
 
     if not characters:
         return ''
-
     password = ''.join(random.choice(characters) for _ in range(length))
     return password
-
-# Example usage
 if __name__ == "__main__":
     print("Generated password:", generate_password(length=16))
